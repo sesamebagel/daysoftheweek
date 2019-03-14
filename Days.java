ed124f002fa80832ba2cb9c4a94075a03aae4b22
sesamebagel <34522551+sesamebagel@users.noreply.github.com>
2019-03-14 16:46:03 -0400

    Create Days.java

---
+++ Days.java
@@ -0,0 +1,10 @@
+package studentdatabase;
+
+public enum Days {
+   SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+
+   @Override
+   public String toString() {
+      return super.toString().substring(0,1) + super.toString().substring(1).toLowerCase();
+   }
+}
